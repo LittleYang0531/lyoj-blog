1a76bb4c00e1314caff3364d107c654134a7a28a
LittleYang0531 <littleyang0531@outlook.com>
2022-08-08 08:04:54 +0000

    Commit

--- api/index.php
+++ api/index.php
@@ -1,12 +1,3 @@
 <?php
-$file= __DIR__ . '/..'.$_SERVER["PHP_SELF"];
-
-if(file_exists($file))
-{
-   return false;
-}
-else
-{
-    require_once __DIR__ . '/../index.php';
-}
+require_once __DIR__ . '/index.php';
 ?>
