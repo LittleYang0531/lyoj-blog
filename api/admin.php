384263dfbb231874b89adf236b85ecbf84f8b3b4
LittleYang0531 <littleyang0531@outlook.com>
2022-08-08 10:38:20 +0000

    Commit

--- api/admin.php
+++ api/admin.php
@@ -3,6 +3,7 @@ $path = $_SERVER['PHP_SELF'];
 $path = substr($path, 7);
 $suffix = explode(".", $path);
 $suffix = $suffix[count($suffix) - 1];
+echo $suffix; exit;
 if ($suffix == "php") require_once(__DIR__ . "/../admin/$path");
 else {
     header("Content-Type: ".mime_content_type($path));
