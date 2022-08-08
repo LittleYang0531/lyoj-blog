f811bfd30031c655bf650ec9acb436351caa6ea1
LittleYang0531 <littleyang0531@outlook.com>
2022-08-08 10:19:36 +0000

    Commit

--- api/admin.php
+++ api/admin.php
@@ -5,6 +5,8 @@ $suffix = explode(".", $path);
 $suffix = $suffix[count($suffix) - 1];
 if ($suffix == "php") require_once(__DIR__ . "/../admin/$path");
 else {
+    header("Content-Type: application/octet-stream");
+    header("Accept-Ranges: bytes");
     $fp = fopen(__DIR__ . "/../admin/$path", "r");
     $cont = fread($fp, filesize(__DIR__ . "/../admin/$path"));
     fclose($fp); echo $cont;
