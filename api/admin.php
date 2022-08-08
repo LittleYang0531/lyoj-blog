a035bacbdc7ee694848af272628a9965931dacf4
LittleYang0531 <littleyang0531@outlook.com>
2022-08-08 10:13:44 +0000

    Commit

--- api/admin.php
+++ api/admin.php
@@ -1,3 +1,12 @@
 <?php
-phpinfo();
+$path = $_SERVER['PHP_SELF'];
+$path = substr($path, 7);
+$suffix = explode(".", $path);
+$suffix = $suffix[count($suffix) - 1];
+if ($suffix == "php") require_once(__DIR__ . "/../admin/$path");
+else {
+    $fp = fopen(__DIR__ . "/../admin/$path", "r");
+    $cont = fread($fp, filesize(__DIR__ . "/../admin/$path"));
+    fclose($fp); echo $cont;
+}
 ?>
