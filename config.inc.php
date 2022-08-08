84ee84a5a14203fe6526beaf9af3d6e65de3fecb
LittleYang0531 <littleyang0531@outlook.com>
2022-08-08 08:08:08 +0000

    Commit

---
+++ config.inc.php
@@ -0,0 +1,40 @@
+<?php
+// site root path
+define('__TYPECHO_ROOT_DIR__', dirname(__FILE__));
+
+// plugin directory (relative path)
+define('__TYPECHO_PLUGIN_DIR__', '/usr/plugins');
+
+// theme directory (relative path)
+define('__TYPECHO_THEME_DIR__', '/usr/themes');
+
+// admin directory (relative path)
+define('__TYPECHO_ADMIN_DIR__', '/admin/');
+
+// register autoload
+require_once __TYPECHO_ROOT_DIR__ . '/var/Typecho/Common.php';
+
+// init
+\Typecho\Common::init();
+
+// config db
+$db = new \Typecho\Db('Mysqli', 'typecho_');
+$db->addServer(array (
+  'host' => '127.0.0.1',
+  'port' => 3306,
+  'user' => 'root',
+  'password' => 'yangweihao20060531',
+  'charset' => 'utf8mb4',
+  'database' => 'typecho',
+  'engine' => 'InnoDB',
+), \Typecho\Db::READ | \Typecho\Db::WRITE);
+// $db->addServer(array (
+//   'host' => '3yshbbnym7dz.ap-northeast-2.psdb.cloud',
+//   'port' => 3306,
+//   'user' => 'f4j146h6r4yv',
+//   'password' => 'pscale_pw_sOtR6JYlWoKkIVPfB_DDsM05eY1exVHSnZpYkYJBq2I',
+//   'charset' => 'utf8mb4',
+//   'database' => 'typecho',
+//   'engine' => 'InnoDB',
+// ), \Typecho\Db::READ | \Typecho\Db::WRITE);
+\Typecho\Db::set($db);
